7d50d4c2956f70f3aebeab1b976b639040d203ee
Rafael Nikolas Puggione <rafaelnikolaspuggi@gmail.com>
2022-10-27 05:21:35 -0300

    Update script.js

--- Estudonauta/Estudos JS/ex015/script.js
+++ Estudonauta/Estudos JS/ex015/script.js
@@ -9,16 +9,22 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             genero = 'Homem'
             if (idade >= 0 && idade < 10) {
                 // Criança
+                img.setAttribute('src', 'criancaH.png')
             } else if (idade < 21) {
                 // Jovem
+                img.setAttribute('src', 'jovemH.png')
             } else if (idade < 50) {
                 // Adulto
+                img.setAttribute('src', 'adultoH.png')
             } else {
                 // Idoso
+                img.setAttribute('src', 'idosoH.png')
             }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
@@ -34,5 +40,6 @@ function verificar() {
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
