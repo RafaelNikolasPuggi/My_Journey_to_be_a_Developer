c46f2375a55f0617582b02c74b9b547e735f3a9a
Rafael Nikolas Puggione <rafaelnikolaspuggi@gmail.com>
2022-12-21 05:28:09 -0300

    Udemy JS

---
+++ Udemy/Luiz Otavio - JavaScript e TypeScript/Curso JS/Aula17/index.js
@@ -0,0 +1,16 @@
+const nome = 'Rafael Nikolas';
+const sobrenome = 'Puggione';
+const idade = 26;
+const peso = 64;
+const alturaEmM = 1.68; // Corrigido para metros (ao invés de cm)
+let indiceMassaCorporal; // peso / (altura * altura)
+let anoNascimento;
+ 
+indiceMassaCorporal = peso / (alturaEmM * alturaEmM);
+anoNascimento = 1996 - idade;
+ 
+// template strings
+ 
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} kg`);
+console.log(`tem ${alturaEmM} de altura e seu IMC é de ${indiceMassaCorporal}`);
+console.log(`${nome} nasceu em ${anoNascimento}.`);
