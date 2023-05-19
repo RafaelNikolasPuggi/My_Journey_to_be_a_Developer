4d311b2c7b1edc0c3ddd6daa8e7f8fe8c221b21d
Rafael Nikolas Puggione <rafaelnikolaspuggi@gmail.com>
2023-05-19 11:12:31 -0300

    Update main.js

--- Alura/JavaScript para Web - Crie páginas dinâmicas/Aluramidi/main.js
+++ Alura/JavaScript para Web - Crie páginas dinâmicas/Aluramidi/main.js
@@ -2,6 +2,6 @@ function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom');onclick = tocaSomPom;
+document.querySelectorAll('.tecla');onclick = tocaSomPom;
 
 
