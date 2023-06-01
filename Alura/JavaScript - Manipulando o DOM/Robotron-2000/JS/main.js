3d2d8604085536b8e9e9a46a4a3b65c25c439537
Rafael Nikolas Puggione <rafaelnikolaspuggi@gmail.com>
2023-06-01 05:19:59 -0300

    Update main.js

--- Alura/JavaScript - Manipulando o DOM/Robotron-2000/JS/main.js
+++ Alura/JavaScript - Manipulando o DOM/Robotron-2000/JS/main.js
@@ -4,8 +4,14 @@ const braco = document.querySelector('#braco');
 
 const controle = document.querySelectorAll('.controle-ajuste')
 
+controle.forEach( (elemento) => {
+    elemento.addEventListener('click', (evento) => {
+        manipulaDados(evento.target.textContent)
+    })        
+})
+
 function manipulaDados(operacao) { 
-    if(operacao === "subtrair") {
+    if(operacao === "-") {
         braco.value = parseInt(braco.value) - 1;
     } 
     else {
