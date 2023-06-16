862d682d9660dbf240fb1794b8e3089df2d31002
Rafael Nikolas Puggione <rafaelnikolaspuggi@gmail.com>
2023-06-16 13:21:33 -0300

    finalização mochila

--- Alura/JavaScript na Web - Armazenando dados no navegador/Mochila/js/main.js
+++ Alura/JavaScript na Web - Armazenando dados no navegador/Mochila/js/main.js
@@ -22,14 +22,12 @@ form.addEventListener('submit', (event) => {
     if (existe) {
         itemAtual.id = existe.id;
         atualizaElemento(itemAtual);
-        itens[existe.id] = itemAtual;
+        itens[itens.findIndex(element => element.id === existe.id)] = itemAtual;
     } else {
-        itemAtual.id = itens.length;
+        itemAtual.id = itens[itens.length - 1] ? (itens[itens.length - 1]).id + 1 : 0;
         criaElemento(itemAtual);
         itens.push(itemAtual);
     }
-
-    itens.push(itemAtual);
 
     localStorage.setItem('itens', JSON.stringify(itens));
 
@@ -46,13 +44,34 @@ function criaElemento(item) {
     numeroItem.innerHTML = item.quantidade;
     numeroItem.dataset.id = item.id;
     numeroItem.innerHTML = item.quantidade;
-
     novoItem.appendChild(numeroItem);
+
     novoItem.innerHTML += item.nome;
+
+    novoItem.appendChild(botaoDeleta(item.id));
 
     lista.appendChild(novoItem);
 }
 
 function atualizaElemento(item) {
     document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
+}
+
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button");
+    elementoBotao.innerHTML = "X";
+    //elementoBotao.setAttribute("data-id", id);
+
+    elementoBotao.addEventListener("click", function() {
+        //const id = this.getAttribute("data-id");
+        deletaElemento(this.parentNode, id);
+    });
+
+    return elementoBotao;
+}
+
+function deletaElemento(tag, id) {
+    tag.remove();
+    itens.splice(itens.findIndex(element => element.id === id), 1);
+    localStorage.setItem('itens', JSON.stringify(itens));
 }
