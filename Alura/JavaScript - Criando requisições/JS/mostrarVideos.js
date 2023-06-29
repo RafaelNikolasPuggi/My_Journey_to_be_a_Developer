807f891d567a409d3e045574e94d08fed6bd42ce
Rafael Nikolas Puggione <rafaelnikolaspuggi@gmail.com>
2023-06-29 05:21:17 -0300

    update - criando requisições

--- Alura/JavaScript - Criando requisições/JS/mostrarVideos.js
+++ Alura/JavaScript - Criando requisições/JS/mostrarVideos.js
@@ -2,22 +2,25 @@ import { conectaApi } from "./conectaAPI.js";
 
 const lista = document.querySelector('[data-lista]');
 
-function constroiCard () {
+function constroiCard (titulo, descricao, url, imagem) {
     const video = document.createElement('li');
     video.className = 'videos__item';
-    video.innerHTML = `<iframe width="100%" height="72%" src="https://www.youtube.com/embed/pA-EgOaF23I"
-    title="YouTube video player" frameborder="0"
+    video.innerHTML = `<iframe width="100%" height="72%" src="${url}"
+    title="${titulo}" frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
     allowfullscreen></iframe>
 <div class="descricao-video">
-    <img src="./img/logo.png" alt="logo canal alura">
-    <h3>Qual é o melhor hardware para programação com Mario Souto</h3>
-    <p>236 mil visualizações</p>
+    <img src="${imagem}" alt="logo canal alura">
+    <h3>${titulo}</h3>
+    <p>${descricao}</p>
 </div>`
 
     return video;
 }
 
 async function listaVideos() {
-    const lista = await conectaApi.listaVideos();
+    const listaAPI = await conectaApi.listaVideos();
+    listaAPI.forEach(element => lista.appendChild(constroiCard(element.titulo, element.descricao, element.url, element.imagem)));
 }
+
+listaVideos();
