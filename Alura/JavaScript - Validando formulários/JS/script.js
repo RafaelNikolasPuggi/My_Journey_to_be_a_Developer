3ee55e1117bc99baed46c5ceca60d4f60992f032
Rafael Nikolas Puggione <rafaelnikolaspuggi@gmail.com>
2023-07-05 05:19:11 -0300

    update Validando Formularios

--- Alura/JavaScript - Validando formulários/JS/script.js
+++ Alura/JavaScript - Validando formulários/JS/script.js
@@ -1,4 +1,5 @@
 import ehUmCpf from "./valida-cpf.js";
+import ehMaiorDeIdade from "./valida-idade.js";
 
 const camposDoFormulario = document.querySelectorAll('[required]');
 
@@ -10,4 +11,8 @@ function verificarCampo(campo){
     if (campo.name == 'cpf' && campo.value.length >= 11){
         ehUmCpf(campo);
     }
+    if (campo.name == 'aniversario' && campo.value != '') {
+        ehMaiorDeIdade(campo);
+    }
 }
+
