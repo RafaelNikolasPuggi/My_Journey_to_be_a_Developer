d287f537353886c71dec59c00b7d7ef3bc75a01a
Rafael Nikolas Puggione <rafaelnikolaspuggi@gmail.com>
2023-07-06 17:26:33 -0300

    update validando formulario

--- Alura/JavaScript - Validando formulários/JS/valida-idade.js
+++ Alura/JavaScript - Validando formulários/JS/valida-idade.js
@@ -1,12 +1,13 @@
+const dataAtual = new Date();
+
 export default function ehMaiorDeIdade(campo) {
     const dataNascimento = new Date(campo.value);
-    if (!validaIdade(dataNascimento)) {
+    if (!validaIdade(dataNascimento) || validaAnoNascimento(dataNascimento)) {
         campo.setCustomValidity('O usuário precisa ser maior de 18 anos de idade.')
     }
 }
 
 function validaIdade(data) {
-    const dataAtual = new Date();
     const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
 
     return dataAtual >= dataMais18;
