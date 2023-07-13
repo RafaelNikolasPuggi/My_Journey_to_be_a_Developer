83fdb620f8c667e044e8ac82d5c949688330e198
Rafael Nikolas Puggione <rafaelnikolaspuggi@gmail.com>
2023-07-13 17:31:52 -0300

    Update sortearNumero.js

--- Alura/JavaScript - Validações e reconhecimento de voz/app/sortearNumero.js
+++ Alura/JavaScript - Validações e reconhecimento de voz/app/sortearNumero.js
@@ -1,6 +1,15 @@
+const menorValor = 1;
+const maiorValor = 3000;
 const numeroSecreto = geraNumeroAleatorio();
 
 function geraNumeroAleatorio() {
-    return parseInt(Math.random() * 100)
+    return parseInt(Math.random() * maiorValor + 1)
 }
 
+const elementoMenorValor = document.getElementById('menorValor');
+elementoMenorValor.innerHTML = menorValor;
+
+const elementoMaiorValor = document.getElementById('maiorValor');
+elementoMaiorValor.innerHTML = maiorValor;
+
+console.log(numeroSecreto);
