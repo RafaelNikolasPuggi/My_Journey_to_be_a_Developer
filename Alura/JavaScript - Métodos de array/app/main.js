27813bd4a836011529cbd3fb9c49a04b1ed9842f
Rafael Nikolas Puggione <rafaelnikolaspuggi@gmail.com>
2023-07-19 05:32:03 -0300

    update metodos arrays

--- Alura/JavaScript - Métodos de array/app/main.js
+++ Alura/JavaScript - Métodos de array/app/main.js
@@ -1,31 +1,12 @@
 let livros = [];
 const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
-const elementoParaInserirLivros = document.getElementById('livros')
 getBuscarLivrosDaAPI();
 
 async function getBuscarLivrosDaAPI() {
     const res = await fetch(endpointDaAPI);
     livros = await res.json();
-    console.log(livros);
-    exibirLivrosNaTela(livros);
+    let livrosComDesconto = aplicaDesconto(livros);
+    exibirLivrosNaTela(livrosComDesconto);
 }
 
-function exibirLivrosNaTela(listaDeLivros) {
-    listaDeLivros.forEach(livro => {
-        elementoParaInserirLivros.innerHTML += 
-        `<div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}
-            ${livro.alt}" />
-        <h2 class="livro__titulo">
-          Vue.js
-          Construa aplicações incríveis
-        </h2>
-        <p class="livro__descricao">Caio Incau</p>
-        <p class="livro__preco" id="preco">R$29,90</p>
-        <div class="tags">
-          <span class="tag">Front-end</span>
-          <span class="tag">Back-end</span>
-        </div>
-      </div>`
-    })
-}
+
--- Alura/JavaScript - Validações e reconhecimento de voz/app/validacao.js
+++ Alura/JavaScript - Validações e reconhecimento de voz/app/validacao.js
@@ -1,26 +1,30 @@
 function verificaSeChutePossuiValorValido(chute) {
-    const numero = +chute;
+    const numero = parseInt(chute);
 
-    if (validaNumero(numero)) {
+    if (isNaN(numero)) {
         elementoChute.innerHTML += '<div>Valor Invalido!</div>';
     }
 
-    if (numeroMaiorOuMenor(numero)) {
+    if (numero > maiorValor || numero < menorValor) {
         elementoChute.innerHTML += `<div>Valor Invalido! O número precisa estar entre o ${menorValor} e ${maiorValor}</div>`
     }
 
     if (numero === numeroSecreto) {
         document.body.innerHTML = `
             <h2>Você acertou!</h2>
-            <h3>O Número Secreto era ${numeroSecreto}.</h3>    
+            <h3>O Número Secreto era ${numeroSecreto}.</h3>
+            
+            <button id="jogar-novamente" class="btn-jogar">Jogar Novamente </button>
         `
+    }else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `<div>O número secreto  é menor <i class="fa-solid fa-down-long"></div>`
+    }else {
+        elementoChute.innerHTML += `<div>O número secreto  é maior <i class="fa-solid fa-up-long"></div>`
     }
 }
 
-function validaNumero(numero) {
-    return Number.isNaN(numero)
-}
-
-function numeroMaiorOuMenor(numero){
-    return numero > maiorValor || numero < menorValor
-}
+document.body.addEventListener('click', (event) => {
+    if (event.target.id =='jogar-novamente') {
+        window.location.reload();
+    }
+})
