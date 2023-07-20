0927d35feaa6fc3d124d81f35254169113c42c6f
Rafael Nikolas Puggione <rafaelnikolaspuggi@gmail.com>
2023-07-20 05:26:30 -0300

    update metodos arrays

--- Alura/JavaScript - Métodos de array/app/metodoFilter.js
+++ Alura/JavaScript - Métodos de array/app/metodoFilter.js
@@ -1,8 +1,8 @@
-const btnFiltrarLivrosDeFront = document.getElementById('btnFiltrarLivrosDeFront');
-
-btnFiltrarLivrosDeFront.addEventListener('click', filtrarLivrosDeFront());
-
-function filtrarLivrosDeFront() {
-    let livrosFiltrados = livros.filter(livro => livro.categoria == 'front-end');
-    console.table(livrosFiltrados);
+const botoes = document.querySelectorAll('.btn');
+botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
+function filtrarLivros() {
+    const elementoBtn = document.getElementById(this.id);
+    const categoria = elementoBtn.value;
+    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria);
+    exibirLivrosNaTela(livrosFiltrados);
 }
