f4afcbab878ac88ae2036732615a4c225b6025de
Rafael Nikolas Puggione <rafaelnikolaspuggi@gmail.com>
2023-09-13 05:29:04 -0300

    update

--- Alura/Bootstrap 5 - Novos recursos e práticas recomendadas de HTML, CSS e JavaScript/script.js
+++ Alura/Bootstrap 5 - Novos recursos e práticas recomendadas de HTML, CSS e JavaScript/script.js
@@ -1,7 +1,12 @@
-const inputCheck = document.getElementById('modo-escuro');
 const elemento = document.querySelector("body");
+const inputCheck = document.querySelector('#modo-noturno');
 
 inputCheck.addEventListener("click", () => {
     const modo = inputCheck.checked ? 'dark' : 'light';
     elemento.setAttribute('data-bs-theme', modo)
 })
+
+
+// document.querySelector("#btn").addEventListener("click", () => {
+// document.body.classList.toggle("dark-mode")
+// })
