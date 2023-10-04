eda6256010c0f37a104ebd7a42779d9d852fdf0f
Rafael Nikolas Puggione <rafaelnikolaspuggi@gmail.com>
2023-10-04 05:22:53 -0300

    update

--- Alura/TypeScript parte 1 - Evoluindo seu JavaScript/app/controllers/negociacao-controller.ts
+++ Alura/TypeScript parte 1 - Evoluindo seu JavaScript/app/controllers/negociacao-controller.ts
@@ -1,24 +1,29 @@
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
+import { NegociacoesView } from "../views/negociacoes-view.js";
+import { MensagemView } from "../views/mensagem-view.js";
 
 export class NegociacaoController {
     private inputData: HTMLInputElement;
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
     private negociacoes = new Negociacoes();
+    private negociacoesView = new NegociacoesView('#negociacoesView');
+    private mensagemView = new MensagemView('#mensagemView');
 
     constructor() {
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
+        this.negociacoesView.update(this.negociacoes)
     }
 
     adiciona(): void{
         const negociacao = this.criaNegociacao();
-        negociacao.data.setDate(12);
         this.negociacoes.adiciona(negociacao);
-        this.negociacoes.lista();
-        console.log(this.negociacoes.lista());
+        //this.negociacoes.lista();
+        this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update('Negociação adicionada com sucesso!');
         this.limparFormulario();
     }
 
