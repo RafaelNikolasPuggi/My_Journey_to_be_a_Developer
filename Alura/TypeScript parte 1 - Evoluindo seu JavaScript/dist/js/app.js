55d062ff837ff56c77525c9d7367aeb4ab47ee53
Rafael Nikolas Puggione <rafaelnikolaspuggi@gmail.com>
2023-10-23 20:37:52 -0300

    update

--- Alura/TypeScript parte 1 - Evoluindo seu JavaScript/dist/js/app.js
+++ Alura/TypeScript parte 1 - Evoluindo seu JavaScript/dist/js/app.js
@@ -5,6 +5,3 @@ form.addEventListener('submit', event => {
     event.preventDefault();
     controller.adiciona();
 });
-//const negociacoesView = new NegociacoesView;
-//const template = negociacoesView.template();
-//console.log(template)
