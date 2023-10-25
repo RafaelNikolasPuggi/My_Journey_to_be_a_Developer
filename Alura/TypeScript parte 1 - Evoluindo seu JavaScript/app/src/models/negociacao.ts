a9cc9647f4268e91422360f343adbfe61d94ea81
Rafael Nikolas Puggione <rafaelnikolaspuggi@gmail.com>
2023-10-24 21:18:26 -0300

    update

--- Alura/TypeScript parte 1 - Evoluindo seu JavaScript/app/src/models/negociacao.ts
+++ Alura/TypeScript parte 1 - Evoluindo seu JavaScript/app/src/models/negociacao.ts
@@ -1,12 +1,11 @@
 export class Negociacao {
-
     constructor(
         private _data: Date, 
         public readonly quantidade: number, 
         public readonly valor: number
     ) {}
 
-    get volume(): Number {
+    get volume(): number {
         return this.quantidade * this.valor;
     }
 
