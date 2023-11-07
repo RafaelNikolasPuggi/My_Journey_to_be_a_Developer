d602aff95ac361f14b18dc1ad9b0a69dfa572e6b
Rafael Nikolas Puggione <rafaelnikolaspuggi@gmail.com>
2023-11-06 21:08:01 -0300

    update ts

---
+++ Alura/TypeScript parte 1 - Evoluindo seu JavaScript/app/dist/js/decorators/escapar.js
@@ -0,0 +1,11 @@
+export function escapar(target, propertyKey, descriptor) {
+    const medodoOriginal = descriptor.value;
+    descriptor.value = function (...args) {
+        let retorno = medodoOriginal.apply(this, args);
+        if (typeof retorno === 'string') {
+            retorno = retorno.replace(/<script>[\s\S]*?<\/script>/, '');
+        }
+        return retorno;
+    };
+    return descriptor;
+}
--- Alura/TypeScript parte 1 - Evoluindo seu JavaScript/app/src/decorators/logar-tempo-de-execucao.ts
+++ Alura/TypeScript parte 1 - Evoluindo seu JavaScript/app/src/decorators/logar-tempo-de-execucao.ts
@@ -1,18 +1,22 @@
-export function logarTempoDeExecucao() {
+export function logarTempoDeExecucao(emSegundos: boolean = false) {
     return function(
         target: any,
         propertyKey: string,
         descriptor: PropertyDescriptor
     ) {
         const medodoOriginal = descriptor.value;
-        descriptor.value = function () {
+        descriptor.value = function (...args: Array<any>) {
+            let divisor = 1;
+            let unidade = 'milisegundos';
+            if (emSegundos) {
+                divisor = 1000;
+                unidade = 'segundos';
+            }
             const t1 = performance.now();
-            const retorno = medodoOriginal();
+            const retorno = medodoOriginal.apply(this, args);
             const t2 = performance.now();
-            console.log(`${propertyKey}, tempo de execução: ${(t2 - t1)/1000} segundos.`)
-        }
-
-
+            console.log(`${propertyKey}, tempo de execução: ${(t2 - t1)/divisor} ${unidade}}.`) 
+        };
         return descriptor;
     }
 }
