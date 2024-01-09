643b65da18697e9d261fb92a5714d87f20f801b4
Rafael Nikolas Puggione <rafaelnikolaspuggi@gmail.com>
2024-01-09 04:51:00 -0300

    update

--- Alura/Vue 3 - Entendendo componentes, diretivas e reatividade no framework/cookin-up/src/components/MostrarReceitas.vue
+++ Alura/Vue 3 - Entendendo componentes, diretivas e reatividade no framework/cookin-up/src/components/MostrarReceitas.vue
@@ -1,3 +1,10 @@
 <template>
-    Mostrando receitas...
+    <section class="receitas">
+        <p class="resultados__receitas">
+            Resultados encontrados: 
+        </p>
+        <p>
+            Veja as opções de receitas que encontramos com os ingredientes que você têm por aí!
+        </p>
+    </section>
 </template>
--- Alura/Vue 3 - Entendendo componentes, diretivas e reatividade no framework/cookin-up/src/http/index.ts
+++ Alura/Vue 3 - Entendendo componentes, diretivas e reatividade no framework/cookin-up/src/http/index.ts
@@ -1,15 +1,15 @@
 import type ICategoria from "@/interfaces/ICategoria";
+import type IReceita from "@/interfaces/IReceita";
 
-export async function obterCategorias() { 
-    const resposta = await fetch('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/07e853b7d0626db51ce2e84bb2f15ca450b7bd7f/categorias.json');
-
-    const categorias: Array<ICategoria> = await resposta.json();
-
-    /* await new Promise((resolver) => {
-      setTimeout(resolver, 3000);
-    })
-    */
-
-    return categorias;
+export async function obterDadosURL<T>(url: string) { 
+    const resposta = await fetch(url);
+    return resposta.json() as T;
   }
   
+  export async function obterCategorias() {
+    return obterDadosURL<ICategoria[]>('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/categorias.json')
+  }
+  
+  export async function obterReceita() {
+    return obterDadosURL<IReceita[]>('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/receitas.json')
+  }
