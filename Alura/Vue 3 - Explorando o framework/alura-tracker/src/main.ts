70c60c7ff42b66fd6fd6dc3784b5e47f51d39000
Rafael Nikolas Puggione <rafaelnikolaspuggi@gmail.com>
2024-03-19 05:24:25 -0300

    update

--- Alura/Vue 3 - Explorando o framework/alura-tracker/src/main.ts
+++ Alura/Vue 3 - Explorando o framework/alura-tracker/src/main.ts
@@ -3,7 +3,11 @@ import App from "./App.vue";
 
 import "@fortawesome/fontawesome-free/css/all.css";
 import roteador from "./roteador";
+import { key, store } from "./store";
 //npm i --save-dev @fortawesome/fontawesome-free
 //https://fontawesome.com/
 
-createApp(App).use(roteador).mount("#app");
+createApp(App)
+.use(roteador)
+.use(store, key)
+.mount("#app");
