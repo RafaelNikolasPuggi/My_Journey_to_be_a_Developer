6013ec9beefbf9fa831274dd53abe52ebf6c2df4
Rafael Nikolas Puggione <rafaelnikolaspuggi@gmail.com>
2024-07-30 08:56:38 -0300

    update

---
+++ Pessoal/my favorite games/src/App.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import GameHeader from './components/GameHeader.vue';
+import GameSideBar from './components/GameSideBar.vue';
+
+export default {
+    components: { GameHeader, GameSideBar }
+  }
+
+</script>
+
+<template>
+  <GameHeader />
+  <GameSideBar />
+
+</template>
+
+<style>
+
+</style>
